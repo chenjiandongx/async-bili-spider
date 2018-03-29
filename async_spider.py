20c711e747a12e7f846cd43e9c13a7049ddf6a2c
chenjiandongx <chenjiandongx@qq.com>
2018-03-30 07:27:14 +0800

    自动循环爬取

--- async_spider.py
+++ async_spider.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import time
+import os
 import json
 import asyncio
-from contextlib import contextmanager
 
 import aiofiles
 from aiohttp import ClientSession
+from python_utls import timeit
 
 
 async def fetch(sem, url, session):
@@ -23,17 +23,18 @@ async def fetch(sem, url, session):
         async with sem:
             async with session.get(url) as response:
                 data = await response.json()
-            if data['code'] == 0:  # 只返回有效数据
+            if data and data.get('code', None) == 0:  # 只返回有效数据
                 return data
     except Exception as e:     # 简单异常处理
         print(e)
 
 
-async def run(count):
+async def run(start, stop):
     """
     运行主函数
 
-    :param count: 迭代次数
+    :param start: range start
+    :param stop: range stop
     :return: 请求数据列表
     """
     url = "http://api.bilibili.com/archive_stat/stat?aid={}"
@@ -44,39 +45,53 @@ async def run(count):
     async with ClientSession() as session:
         tasks = [
             asyncio.ensure_future(fetch(sem, url.format(i), session))
-            for i in range(count)]
+            for i in range(start, stop)]
         # 使用 gather(*tasks) 收集数据，wait(tasks) 不收集数据
         return await asyncio.gather(*tasks)
 
 
-async def save(count, path='bili.json'):
+async def save(start, stop, path, label):
     """
     异步存储文件
 
-    :param count: 迭代次数
+    :param start: range start
+    :param stop: range stop
     :param path: 文件路径
+    :param label: 任务名称
     """
-
-    data = await asyncio.gather(asyncio.ensure_future(run(count)))
+    print("启动: 任务 {}".format(label))
+    data = await asyncio.gather(asyncio.ensure_future(run(start, stop)))
     result = [d for d in data[0] if d]
     async with aiofiles.open(path, mode='w+') as f:
         await f.write(json.dumps(result))
         print('爬取: {} 条数据'.format(len(result)))
 
 
-# 上下文管理器，用与计算耗时
-@contextmanager
-def timer():
-    start = time.time()
-    try:
-        yield
-    finally:
-        print('耗时: {} 秒'.format(time.time() - start))
-
-
 if __name__ == "__main__":
-    MAX_CONNECT_COUNT = 1000    # 最大并发数
-    NUMBER = 2000
-    with timer():
+    MAX_CONNECT_COUNT = 1024    # 最大并发数
+    NUMBER = 100
+    with timeit.timeit_block('m'):
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(asyncio.ensure_future(save(NUMBER)))
+        # index = [(16, 17), (17, 18)]
+        # tasks = [
+        #     asyncio.ensure_future(
+        #         save(
+        #             start=NUMBER * i[0],
+        #             stop=NUMBER * i[1],
+        #             path=os.path.join("json", "{}.json".format(NUMBER * i[1])),
+        #             label=i[1]
+        #         ))
+        #     for i in index]
+        # loop.run_until_complete(asyncio.gather(*tasks))
+        #
+        for i in range(0, 18, 2):
+            tasks = [
+                asyncio.ensure_future(
+                    save(
+                        start=NUMBER * i[0],
+                        stop=NUMBER * i[1],
+                        path=os.path.join("data", "{}.json".format(NUMBER * i[1])),
+                        label=i[1]
+                    ))
+                for i in [(i, i + 1), (i + 1, i + 2)]]
+            loop.run_until_complete(asyncio.gather(*tasks))
