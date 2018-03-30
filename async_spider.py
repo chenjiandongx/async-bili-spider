a329fa1d8326f4a11ac31a8331b4929e8f24ba5a
chenjiandongx <chenjiandongx@qq.com>
2018-03-30 15:42:39 +0800

    完全异步编程

--- async_spider.py
+++ async_spider.py
@@ -2,10 +2,12 @@
 # coding=utf-8
 
 import os
+import time
 import json
 import asyncio
 
 import aiofiles
+import aiomysql
 from aiohttp import ClientSession
 from python_utls import timeit
 
@@ -50,9 +52,9 @@ async def run(start, stop):
         return await asyncio.gather(*tasks)
 
 
-async def save(start, stop, path, label):
+async def save_to_files(start, stop, path, label):
     """
-    异步存储文件
+    异步存储数据至文件
 
     :param start: range start
     :param stop: range stop
@@ -64,34 +66,109 @@ async def save(start, stop, path, label):
     result = [d for d in data[0] if d]
     async with aiofiles.open(path, mode='w+') as f:
         await f.write(json.dumps(result))
-        print('爬取: {} 条数据'.format(len(result)))
+        print('存储: {} 条数据'.format(len(result)))
+
+
+async def save_to_database(start, stop, label, loop):
+    """
+    异步存储数据至数据库
+
+    :param start: range start
+    :param stop: range stop
+    :param label: 任务名称
+    :param loop: 循环事件
+
+    建表语句，只需建一次所以直接执行即可
+    create table if not exists bili_video(
+        v_aid int primary key,
+        v_view int,
+        v_danmaku int,
+        v_reply int,
+        v_favorite int,
+        v_coin int,
+        v_share int,
+        v_name text);
+    """
+    print("启动: 任务 {}".format(label))
+    data = await asyncio.gather(asyncio.ensure_future(run(start, stop)))
+    result = [
+        (
+            row['data']['aid'],         # 视频编号
+            row['data']['view'],        # 播放量
+            row['data']['danmaku'],     # 弹幕数
+            row['data']['reply'],       # 评论数
+            row['data']['favorite'],    # 收藏数
+            row['data']['coin'],        # 硬币数
+            row['data']['share'],       # 分享数
+            ""                  # 视频名称（暂时为空）
+        )
+        for row in data[0]
+        if row and row['data']['view'] != "--" and row['data']['aid'] != 0]
+    conn = await aiomysql.connect(
+        host='127.0.0.1',
+        port=3306,
+        user='root',
+        password='0303',
+        db='chenx',
+        loop=loop)
+
+    async with conn.cursor() as cur:
+        await cur.executemany(
+            "INSERT INTO bili_video (v_aid, v_view, "
+            "v_danmaku, v_reply, v_favorite, v_coin, v_share, v_name)"
+            "values (%s, %s, %s, %s, %s, %s, %s, %s)", result)
+        await conn.commit()
+    conn.close()
+    print('存储: {} 条数据'.format(len(result)))
+
+
+def get_files_tasks(index):
+    """
+    获取 `save_to_files()` 所需任务
+
+    :param index: 任务索引
+    :return 任务列表
+    """
+    _tasks = [
+        asyncio.ensure_future(
+            save_to_files(
+                start=NUMBER * i[0],
+                stop=NUMBER * i[1],
+                path=os.path.join("json", "{}.json".format(NUMBER * i[1])),
+                label=i[1])
+        ) for i in [(index, index + 1), (index + 1, index + 2)]
+    ]
+    return _tasks
+
+
+def get_database_tasks(index, loop):
+    """
+    获取 `save_to_database()` 所需任务
+
+    :param index: 任务索引
+    :param loop: 事件循环
+    :return 任务列表
+    """
+    _tasks = [
+        asyncio.ensure_future(
+            save_to_database(
+                start=NUMBER * i[0],
+                stop=NUMBER * i[1],
+                label=i[1],
+                loop=loop)
+        ) for i in [(index, index + 1), (index + 1, index + 2)]
+    ]
+    return _tasks
 
 
 if __name__ == "__main__":
     MAX_CONNECT_COUNT = 1024    # 最大并发数
-    NUMBER = 100
-    with timeit.timeit_block('m'):
+    NUMBER = 10000 * 50         # 单任务爬取数
+    with timeit.timeit_block('h'):
         loop = asyncio.get_event_loop()
-        # index = [(16, 17), (17, 18)]
-        # tasks = [
-        #     asyncio.ensure_future(
-        #         save(
-        #             start=NUMBER * i[0],
-        #             stop=NUMBER * i[1],
-        #             path=os.path.join("json", "{}.json".format(NUMBER * i[1])),
-        #             label=i[1]
-        #         ))
-        #     for i in index]
-        # loop.run_until_complete(asyncio.gather(*tasks))
-        #
-        for i in range(0, 18, 2):
-            tasks = [
-                asyncio.ensure_future(
-                    save(
-                        start=NUMBER * i[0],
-                        stop=NUMBER * i[1],
-                        path=os.path.join("data", "{}.json".format(NUMBER * i[1])),
-                        label=i[1]
-                    ))
-                for i in [(i, i + 1), (i + 1, i + 2)]]
-            loop.run_until_complete(asyncio.gather(*tasks))
+        for index in range(0, 42, 2):
+            with timeit.timeit_block('m'):
+                tasks = get_database_tasks(index, loop)
+                loop.run_until_complete(asyncio.gather(*tasks))
+            print('Take a deep breath: 30s')
+            time.sleep(30)
