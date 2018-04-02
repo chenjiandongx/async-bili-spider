94c52bc007613f4867ca645c7778bd76811517a4
chenjiandongx <chenjiandongx@qq.com>
2018-04-02 16:04:39 +0800

    white 代码格式整理

--- async_spider.py
+++ async_spider.py
@@ -25,8 +25,9 @@ async def fetch(sem, url, session):
         async with sem:
             async with session.get(url) as response:
                 data = await response.json()
-            if data and data.get('code', None) == 0:  # 只返回有效数据
+            if data and data.get("code", None) == 0:  # 只返回有效数据
                 return data
+
     except Exception as e:
         print(e)
 
@@ -47,7 +48,8 @@ async def run(start, stop):
     async with ClientSession() as session:
         tasks = [
             asyncio.ensure_future(fetch(sem, url.format(i), session))
-            for i in range(start, stop)]
+            for i in range(start, stop)
+        ]
         # 使用 gather(*tasks) 收集数据，wait(tasks) 不收集数据
         return await asyncio.gather(*tasks)
 
@@ -61,12 +63,12 @@ async def save_to_files(start, stop, path, label):
     :param path: 文件路径
     :param label: 任务名称
     """
-    print("Running: job {}".format(label))
+    print(f"Running: job {label}")
     data = await asyncio.gather(asyncio.ensure_future(run(start, stop)))
     result = [d for d in data[0] if d]
-    async with aiofiles.open(path, mode='w+') as f:
+    async with aiofiles.open(path, mode="w+") as f:
         await f.write(json.dumps(result))
-        print('Fetch data: {}'.format(len(result)))
+        print(f"Fetch data: {len(result)}")
 
 
 async def save_to_database(start, stop, label, loop):
@@ -89,37 +91,41 @@ async def save_to_database(start, stop, label, loop):
         v_share int,
         v_name text);
     """
-    print("Running: job {}".format(label))
+    print(f"Running: job {label}")
     data = await asyncio.gather(asyncio.ensure_future(run(start, stop)))
     result = [
         (
-            row['data']['aid'],         # 视频编号
-            row['data']['view'],        # 播放量
-            row['data']['danmaku'],     # 弹幕数
-            row['data']['reply'],       # 评论数
-            row['data']['favorite'],    # 收藏数
-            row['data']['coin'],        # 硬币数
-            row['data']['share'],       # 分享数
-            ""                  # 视频名称（暂时为空）
+            row["data"]["aid"],  # 视频编号
+            row["data"]["view"],  # 播放量
+            row["data"]["danmaku"],  # 弹幕数
+            row["data"]["reply"],  # 评论数
+            row["data"]["favorite"],  # 收藏数
+            row["data"]["coin"],  # 硬币数
+            row["data"]["share"],  # 分享数
+            "",  # 视频名称（暂时为空）
         )
         for row in data[0]
-        if row and row['data']['view'] != "--" and row['data']['aid'] != 0]
+        if row and row["data"]["view"] != "--" and row["data"]["aid"] != 0
+    ]
     conn = await aiomysql.connect(
-        host='127.0.0.1',
+        host="127.0.0.1",
         port=3306,
-        user='root',
-        password='0303',
-        db='chenx',
-        loop=loop)
+        user="root",
+        password="0303",
+        db="chenx",
+        loop=loop,
+    )
 
     async with conn.cursor() as cur:
         await cur.executemany(
             "INSERT INTO bili_video (v_aid, v_view, "
             "v_danmaku, v_reply, v_favorite, v_coin, v_share, v_name)"
-            "values (%s, %s, %s, %s, %s, %s, %s, %s)", result)
+            "values (%s, %s, %s, %s, %s, %s, %s, %s)",
+            result,
+        )
         await conn.commit()
     conn.close()
-    print('Fetch data: {}'.format(len(result)))
+    print(f"Fetch data: {len(result)}")
 
 
 def get_files_tasks(index):
@@ -135,8 +141,10 @@ def get_files_tasks(index):
                 start=NUMBER * i[0],
                 stop=NUMBER * i[1],
                 path=os.path.join("json", "{}.json".format(NUMBER * i[1])),
-                label=i[1])
-        ) for i in [(index, index + 1), (index + 1, index + 2)]
+                label=i[1],
+            )
+        )
+        for i in [(index, index + 1), (index + 1, index + 2)]
     ]
     return _tasks
 
@@ -152,24 +160,23 @@ def get_database_tasks(index, loop):
     _tasks = [
         asyncio.ensure_future(
             save_to_database(
-                start=NUMBER * i[0],
-                stop=NUMBER * i[1],
-                label=i[1],
-                loop=loop)
-        ) for i in [(index, index + 1), (index + 1, index + 2)]
+                start=NUMBER * i[0], stop=NUMBER * i[1], label=i[1], loop=loop
+            )
+        )
+        for i in [(index, index + 1), (index + 1, index + 2)]
     ]
     return _tasks
 
 
 if __name__ == "__main__":
-    MAX_CONNECT_COUNT = 1024    # 最大并发数
-    NUMBER = 10000 * 50         # 单任务爬取数
-    with timeit.timeit_block('h'):
+    MAX_CONNECT_COUNT = 1024  # 最大并发数
+    NUMBER = 10000 * 50  # 单任务爬取数
+    with timeit.timeit_block("h"):
         loop = asyncio.get_event_loop()
         for index in range(0, 42, 2):
-            with timeit.timeit_block('m'):
+            with timeit.timeit_block("m"):
                 tasks = get_database_tasks(index, loop)
                 # tasks = get_files_tasks(index)
                 loop.run_until_complete(asyncio.gather(*tasks))
-            print('Take a deep breath: 30s')
+            print("Take a deep breath: 30s")
             time.sleep(30)
--- async_video_name.py
+++ async_video_name.py
@@ -8,16 +8,14 @@ import pyquery
 import aiomysql
 
 
-COLUMNS = [
-    "v_view", "v_danmaku", "v_reply", "v_favorite", "v_coin", "v_share",
-]
+COLUMNS = ["v_view", "v_danmaku", "v_reply", "v_favorite", "v_coin", "v_share"]
 
-HOST = '127.0.0.1'
+HOST = "127.0.0.1"
 PORT = 3306
-USER = 'root'
-PASSWORD = '0303'
-DATABASE = 'chenx'
-CHARSET = 'utf8'
+USER = "root"
+PASSWORD = "0303"
+DATABASE = "chenx"
+CHARSET = "utf8"
 
 
 async def get_video_aid(col, loop):
@@ -34,12 +32,11 @@ async def get_video_aid(col, loop):
         password=PASSWORD,
         db=DATABASE,
         charset=CHARSET,
-        loop=loop)
+        loop=loop,
+    )
 
     async with conn.cursor() as cur:
-        sql = ("select v_aid "
-               "from bili_video "
-               "order by {} desc limit 100".format(col))
+        sql = f"select v_aid from bili_video order by {col} desc limit 100"
         await cur.execute(sql)
         result = await cur.fetchall()
     conn.close()
@@ -53,12 +50,13 @@ async def fetch(aid, session):
     :param aid: 视频 id
     :param session: Session 实例
     """
-    url = "https://www.bilibili.com/video/av{}".format(aid)
+    url = f"https://www.bilibili.com/video/av{aid}"
     try:
         async with session.get(url) as response:
             req = await response.text()
             q = pyquery.PyQuery(req)
             return {aid: q("h1[title]").text()}
+
     except Exception as e:
         print(e)
 
@@ -72,8 +70,7 @@ async def get_video_name(col, loop):
     """
     result = await get_video_aid(col, loop)
     async with ClientSession() as session:
-        tasks = [
-            asyncio.ensure_future(fetch(i[0], session)) for i in result]
+        tasks = [asyncio.ensure_future(fetch(i[0], session)) for i in result]
         # 使用 gather(*tasks) 收集数据，wait(tasks) 不收集数据
         return await asyncio.gather(*tasks)
 
@@ -93,16 +90,17 @@ async def save_to_database(col, loop):
         password=PASSWORD,
         db=DATABASE,
         charset=CHARSET,
-        loop=loop)
+        loop=loop,
+    )
 
-    result = [
-        (v_name, v_aid) for r in result for v_aid, v_name in r.items()]
+    result = [(v_name, v_aid) for r in result for v_aid, v_name in r.items()]
     async with conn.cursor() as cur:
         await cur.executemany(
-            "update bili_video set v_name = %s where v_aid = %s", result)
+            "update bili_video set v_name = %s where v_aid = %s", result
+        )
         await conn.commit()
     conn.close()
-    print("{} DONE!!!".format(col))
+    print(f"{col} DONE!!!")
 
 
 if __name__ == "__main__":
